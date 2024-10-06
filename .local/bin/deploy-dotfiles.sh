16529d9840bf102d8ac3e06107cef596ab880b35
Eike Flath <eike9008@gmail.com>
2024-10-06 17:55:54 +0200

    Added user creation

---
+++ .local/bin/deploy-dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+die() {
+	echo "$1"
+	exit 1
+}
+
+[ $(id -u) -eq 0 ] || die "run this as root"
+[ -n "${1-}" ] || die "usage: $0 <user name>"
+
+DOTFILES_ROOT="$(realpath "$(dirname "$0")/../..")"
+echo "dotfiles root: $DOTFILES_ROOT"
+
+echo "checking for internet access..."
+ping -c 1 openbsd.org >/dev/null || die "failed to ping openbsd.org"
+
+USER_NAME="$1"
+userinfo -e "$USER_NAME" && die "the user $USER_NAME already exists" 
+groupinfo -e users || die "group users does not exist"
+groupinfo -e wheel || die "group users does not exist"
+
+set -x
+useradd -m -s "$SHELL" -k "$DOTFILES_ROOT" -g users -G wheel
+passwd "$USER_NAME"
+
