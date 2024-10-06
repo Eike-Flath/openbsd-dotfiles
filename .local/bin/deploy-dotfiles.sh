815057c713288d2b2d6f4e6ea3e1a7be81c96580
Eike Flath <eike9008@gmail.com>
2024-10-06 18:07:19 +0200

    don't add certain files to the user home

--- .local/bin/deploy-dotfiles.sh
+++ .local/bin/deploy-dotfiles.sh
@@ -24,4 +24,10 @@ groupinfo -e wheel || die "group users does not exist"
 set -x
 useradd -m -s "$SHELL" -k "$DOTFILES_ROOT" -g users -G wheel "$USER_NAME"
 passwd "$USER_NAME"
+set +x
+USER_HOME="$(userinfo "$USER_NAME" | grep ^dir | cut -f 2)"
+for f in $(<"$DOTFILES_ROOT/.dotfileignore")
+do
+	rm -rfv "$USER_HOME/$f"
+done
 
