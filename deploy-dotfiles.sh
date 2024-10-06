f3fc2167210ed04b9f9fe3c40bd27b01fb76c496
Eike Flath <eike9008@gmail.com>
2024-10-06 19:38:16 +0200

    install git & htop on deployment

--- deploy-dotfiles.sh
+++ deploy-dotfiles.sh
@@ -12,24 +12,33 @@ die() {
 [ -n "${1-}" ] || die "usage: $0 <user name>"
 
 DOTFILES_ROOT="$(realpath "$(dirname "$0")")"
-echo "dotfiles root: $DOTFILES_ROOT"
 
 echo "checking for internet access..."
 ping -c 1 openbsd.org >/dev/null || die "failed to ping openbsd.org"
+clear
 
+echo "installing some software..."
+pkg_add git htop
+clear
+
+echo "creating user..."
 USER_NAME="$1"
 userinfo -e "$USER_NAME" && die "the user $USER_NAME already exists" 
 groupinfo -e users || die "group users does not exist"
 groupinfo -e wheel || die "group wheel does not exist"
 [ -x /bin/ksh ] || die "/bin/ksh doesn't exist"
-
 set -x
 useradd -m -s /bin/ksh -k "$DOTFILES_ROOT" -g users -G wheel "$USER_NAME"
 passwd "$USER_NAME"
 set +x
+clear
+
 USER_HOME="$(userinfo "$USER_NAME" | grep ^dir | cut -f 2)"
 for f in $(<"$DOTFILES_ROOT/.dotfileignore")
 do
 	rm -rf "$USER_HOME/$f"
 done
+
+echo "login as $USER_NAME to finish installation" 
+
 
